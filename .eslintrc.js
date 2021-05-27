224e5a5b0539ec1d1563e81ad385a075f8a381d7
gness1804 <graham.nessler@gmail.com>
2021-05-26 19:42:56 -0500

    Add eslint, prettier, other deps

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2017,
+  },
+  extends: ['eslint:recommended'],
+  rules: {
+    'no-console': 'warn',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  settings: {
+    'import/extensions': ['.mjs', '.js'],
+    'import/resolver': {
+      node: {
+        extensions: ['.mjs', '.js'],
+      },
+    },
+  },
+};
