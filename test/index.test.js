27ef4d7676fb19ba22bbb5aaa24a02badc2f5434
gness1804 <graham.nessler@gmail.com>
2021-06-30 19:59:13 -0500

    🐛 FIX: tests

--- test/index.test.js
+++ test/index.test.js
@@ -13,35 +13,35 @@ test.beforeEach(() => {
 });
 
 test('fails if no argument given', async (t) => {
-  const { stderr } = await execa('node', [program]);
+  const { stdout } = await execa('node', [program]);
   t.true(
-    stderr.includes(
+    stdout.includes(
       'Error: Input needed. Please enter a valid file or directory name.\n',
     ),
   );
 });
 
 test('fails if bad argument given (not a file or directory)', async (t) => {
-  const { stderr } = await execa('node', [program, 'foo/bar']);
+  const { stderr } = await execa('node', [program, '-s', 'foo/bar']);
   t.true(stderr.includes('Failed to parse foo/bar'));
 });
 
 test('converts a valid HEIC to jpg', async (t) => {
   const source = path.join(__dirname, 'fixtures/ahc_pic_1.HEIC');
-  await execa('node', [program, source]);
+  await execa('node', [program, '-s', source]);
   const res = path.join(__dirname, 'fixtures/ahc_pic_1.jpg');
   t.true(fs.existsSync(res));
 });
 
 test('fails if non-HEIC file argument given', async (t) => {
   const source = path.join(__dirname, 'fixtures/nope.md');
-  const { stderr } = await execa('node', [program, source]);
+  const { stderr } = await execa('node', [program, '-s', source]);
   t.true(stderr.includes('File path must be of type .HEIC.'));
 });
 
 test('converts all HEICs in a valid directory', async (t) => {
   const source = path.join(__dirname, 'fixtures/');
-  await execa('node', [program, source]);
+  await execa('node', [program, '-s', source]);
   const file1 = path.join(__dirname, 'fixtures/ahc_pic_1.jpg');
   const file2 = path.join(__dirname, 'fixtures/ahc-pic-bad.jpg');
   const file3 = path.join(__dirname, 'fixtures/enchanted-rock.jpg');
