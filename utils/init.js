ec223703651fd4c304c4c59596fcaf41bc902049
Graham Nessler <grahamnessler@insiderx.com>
2021-07-06 14:37:48 -0500

    👌 IMPROVE:  chore: add package notifier and welcome text to init

--- utils/init.js
+++ utils/init.js
@@ -1,4 +1,7 @@
 const unhandled = require('cli-handle-unhandled');
+const updateNotifier = require('update-notifier');
+const welcome = require('cli-welcome');
+const pkg = require('../package.json');
 
 module.exports = () => {
   // check node version; fail if before 14
@@ -10,4 +13,16 @@ module.exports = () => {
 
   // handle unhandled errors
   unhandled();
+  welcome({
+    title: pkg.name,
+    tagLine: `by ${pkg.author}`,
+    description: pkg.description,
+    version: pkg.version,
+    bgColor: '#c45f12',
+    color: '#000',
+    bold: true,
+    clear: true,
+  });
+
+  updateNotifier({ pkg }).notify();
 };
